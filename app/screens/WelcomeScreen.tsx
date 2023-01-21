f1e3784423437a74e08ac9fa83226db0f75fd7dc
Jonathan Levinn <jon.levin25@gmail.com>
2023-01-22 01:06:58 +0200

    WelcomeScreen: rewrite (see mosh solution)

--- app/screens/WelcomeScreen.tsx
+++ app/screens/WelcomeScreen.tsx
@@ -1,50 +1,61 @@
-import React, {} from 'react';
-import {Image, View, Text, } from 'react-native';
+import React from 'react'
+import { Text, StyleSheet, View, ImageBackground, Image } from 'react-native'
 
-export default function WelcomeScreen(props: any) {
-    return <>
-      <>
-      <Image 
-        source={require('../assets/background.jpg')}
-        style={{
-          flex: 0.8,
-          resizeMode: "contain",
-        }}
-        />
-        <View style={{
-          flex: 0.1,
-          backgroundColor: 'tomato',
-          width: '100%',
-        }}/>
-        <View style={{
-          flex: 0.1,
-          backgroundColor: 'dodgerblue',
-          width: '100%',
-        }}/>
-        </>
-        <View style={{
-          top: 50,
-          position: "absolute",
-          // backgroundColor: "gold",
-          // justifyContent: "flex-start"
-        }}>
-          <Image
-          source={require('../assets/logo-red.png')}
-          style={{
-            height: 100,
-            width: 100,
-            alignSelf: "center",
-            // resizeMode: "contain"
-          }}/>
-          <Text style={{
-            flex: 1,
-            textAlign: "center",
-            fontSize: 14,
-            // fontFamily: ""
-          }}>
-            Sell what you don't need
-          </Text>
-        </View>
-      </>
-  }
+function WelcomeScreen(props: any) {
+return (
+    <>
+        <ImageBackground style={styles.background} source={require('../assets/background.jpg')}>
+            {/* <View style={styles.contentContainer}/> */}
+            <View style={styles.loginBtn}/>
+            <View style={styles.registerBtn}/>
+            <View style={styles.logoContainer}>
+                <Image style={styles.logoImg} source={require('../assets/logo-red.png')}/>
+                <Text style={styles.logoTxt}>Sell what you don't need!</Text>
+            </View>
+        </ImageBackground>
+        
+    </>
+
+    );
+}
+
+const genericStyles = StyleSheet.create({
+    btn: {
+        width: "100%",
+        height: 70,
+    },
+
+})
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        width: "100%",
+        justifyContent: "flex-end",
+        alignItems: "center",
+    },
+    logoImg: {
+        width: 100,
+        height: 100,
+    },
+    logoTxt: {
+        // position: "absolute",
+        // top: 100,
+        // overflow: 'visible',
+    },
+    logoContainer: {
+        top: 50,
+        position: "absolute",
+        alignItems: "center",
+    },
+    registerBtn: {
+        ...genericStyles.btn,
+        backgroundColor: "#4ecdc4",
+    },
+    loginBtn: {
+        ...genericStyles.btn,
+        backgroundColor: "#fc5c65",
+    }
+})
   
+export default WelcomeScreen;

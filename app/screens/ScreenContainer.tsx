ec32fbc48bc2aa806660eb76eaa08d650763831f
Jonathan Levinn <jon.levin25@gmail.com>
2023-01-25 17:23:58 +0200

    ScreenContainer: use Expo constants

--- app/screens/ScreenContainer.tsx
+++ app/screens/ScreenContainer.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, SafeAreaView, View } from 'react-native';
+import Constants from 'expo-constants';
 
 type ScreenContainerProps = {
     children?: React.ReactNode;
 };
 
 function ScreenContainer({children}: ScreenContainerProps) {
-    const container = Platform.select({
-        ios: <SafeAreaView>{children}</SafeAreaView>,
-        android: <View style={{
-            // backgroundColor: "green",
-            width: "100%",
-            height: "100%",
-            top: StatusBar.currentHeight}}>{children}</View>,
-    });
-    if (container == null) {
-        throw new Error(`Unknown Platform! ${Platform.OS}`);
-    }
-    return container;
+    return (
+        <SafeAreaView style={styles.screen}>
+            {children}
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        paddingTop: Constants.statusBarHeight, // on ios - should be a no-op compared to regular SafeAreaView
+    },
+})
 
 export default ScreenContainer;

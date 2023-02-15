d8f96c5e81ca9673e589a6ad98971cc3bc922d9a
Jonathan Levin <jon.levin25@gmail.com>
2023-02-15 17:15:08 +0200

    recreate flatlist

---
+++ app/screens/MessageListScreen.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, View, FlatList } from "react-native";
+import ListItem from "../components/ListItem";
+import ListSeparator from "../components/ListSeparator";
+import AppText from "../components/AppText";
+const styles = StyleSheet.create({});
+
+import { messagesData } from "../data/mockData";
+
+
+// flat list, uses messagesData
+
+function MessageListScreen() {
+  return (
+    
+  );
+}
+
+export default MessageListScreen;
--- excersize_3.tsx
+++ excersize_3.tsx
@@ -13,28 +13,27 @@ import {
 } from "react-native";
 
 import { AppColors } from "./app/AppStyles";
-import { itemJacket, listingJacket, userMosh, messagesData } from "./app/data/mockData";
+import {
+  itemJacket,
+  listingJacket,
+  userMosh,
+  messagesData,
+} from "./app/data/mockData";
 import ScreenContainer from "./app/screens/ScreenContainer";
-import ListItem from './app/components/ListItem';
+import ListItem from "./app/components/ListItem";
 import { MessageData } from "./app/data/MessageData";
 import ListSeparator from "./app/components/ListSeparator";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessageListScreen from "./app/screens/MessageListScreen";
 
 export default function App() {
-  
-  return <ScreenContainer>
-    {/* <WelcomeScreen />; */}
-    {/* <TestCardScreen />; */}
-    {/* <ViewImageScreen />; */}
-    {/* <ListingDetailsScreen listing={listingJacket} />; */}
-    <FlatList
-    data={messagesData}
-    renderItem={({item}) => 
-      (<ListItem
-        renderRightActions={() => <View style={{backgroundColor:"tomato", width: 70}}/>}
-        onPress={() => console.log(`onPress ${item}`)}
-      title={item.title} subtitle={item.description} image={item.image}/>)}
-    keyExtractor={(item) => item.id}
-    ItemSeparatorComponent={() => <ListSeparator/>}>)
-    </FlatList>
-  </ScreenContainer>
+  return (
+    <ScreenContainer>
+      {/* <WelcomeScreen /> */}
+      {/* <TestCardScreen /> */}
+      {/* <ViewImageScreen /> */}
+      {/* <ListingDetailsScreen listing={listingJacket} /> */}
+      <MessageListScreen />
+    </ScreenContainer>
+  );
 }

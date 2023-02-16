3f5457cc65d676205075e71f549e26acb1d04bbc
Jonathan Levin <jon.levin25@gmail.com>
2023-02-16 23:50:18 +0200

    WIP: Delete button (todo: look into animating props on renderRight

--- app/screens/MessageListScreen.tsx
+++ app/screens/MessageListScreen.tsx
@@ -1,13 +1,44 @@
 import React from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import ListItem from "../components/ListItem";
 import ListSeparator from "../components/ListSeparator";
 import AppText from "../components/AppText";
-const styles = StyleSheet.create({});
 
 import { messagesData } from "../data/mockData";
+import { MessageData } from "../data/MessageData";
+import { Swipeable } from "react-native-gesture-handler";
+import { AppColors } from "../AppStyles";
+import AppButton from "../components/AppButton";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 // flat list, uses messagesData
+type DeleteWidgetProps = { msg: MessageData; drag: number };
+
+function DeleteWidget({ msg, drag }: DeleteWidgetProps) {
+  const deleteMsg = () => {
+    console.log(`Deleting ${msg}!`);
+  };
+  const maxWidth = styles.deleteContainer.width;
+  const width = Math.min(Math.abs(drag), maxWidth);
+  return (
+    <TouchableOpacity
+      // style={[styles.deleteContainer, { width }]}
+      style={styles.deleteContainer}
+      onPress={deleteMsg}
+    >
+      <MaterialCommunityIcons
+        name="trash-can-outline"
+        style={styles.trashIcon}
+      />
+    </TouchableOpacity>
+  );
+}
 
 function MessageListScreen() {
   return (
@@ -20,11 +51,32 @@ function MessageListScreen() {
           subtitle={item.description}
           image={item.image}
           onPress={() => console.log(item)}
+          renderRightActions={(progress, drag, swipable) => {
+            console.log(
+              `prog: ${JSON.stringify(progress)} drag: ${JSON.stringify(drag)}`
+            );
+            return <DeleteWidget drag={drag} msg={item} />;
+          }}
         />
       )}
       ItemSeparatorComponent={ListSeparator}
     ></FlatList>
   );
 }
+
+const styles = StyleSheet.create({
+  deleteContainer: {
+    width: 60,
+    height: "100%",
+    backgroundColor: AppColors.danger,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  trashIcon: {
+    alignSelf: "center",
+    color: AppColors.white,
+    fontSize: 22,
+  },
+});
 
 export default MessageListScreen;
--- app/screens/ScreenContainer.tsx
+++ app/screens/ScreenContainer.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, SafeAreaView, View } from 'react-native';
-import Constants from 'expo-constants';
+import React from "react";
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  View,
+} from "react-native";
+import Constants from "expo-constants";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 type ScreenContainerProps = {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 };
 
-function ScreenContainer({children}: ScreenContainerProps) {
-    return (
-        <SafeAreaView style={styles.screen}>
-            {children}
-        </SafeAreaView>
-    );
+function ScreenContainer({ children }: ScreenContainerProps) {
+  return (
+    <SafeAreaView style={styles.screen}>
+      <GestureHandlerRootView>{children}</GestureHandlerRootView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        paddingTop: Constants.statusBarHeight, // on ios - should be a no-op compared to regular SafeAreaView
-    },
-})
+  screen: {
+    paddingTop: Constants.statusBarHeight, // on ios - should be a no-op compared to regular SafeAreaView
+  },
+});
 
 export default ScreenContainer;

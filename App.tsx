3915d7dfeae77f940a06276a3d823d9c0a7407ca
Jonathan Levin <jon.levin25@gmail.com>
2023-02-19 20:08:39 +0200

    remove 'excersize' screens, put in App

--- App.tsx
+++ App.tsx
@@ -1,5 +1,47 @@
-import Excersize_1 from "./excersize_1";
-import Excersize_2 from "./excersize_2";
-import Excersize_3 from "./excersize_3";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import { Text, StyleSheet, ListRenderItemInfo, View } from "react-native";
 
-export default Excersize_3;
+// screens
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ScreenContainer from "./app/screens/ScreenContainer";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessageListScreenApp from "./app/screens/MessageListScreen";
+import TestCardScreen from "./app/screens/TestCardScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessageListScreen from "./app/screens/MessageListScreen";
+import MyAccountScreen from "./app/screens/MyAccountScreen";
+
+// data/config
+import { AppColors } from "./app/AppStyles";
+import { listingJacket } from "./app/data/mockData";
+
+export default function App() {
+  return (
+    <ScreenContainer style={styles.container}>
+      {/* <View
+        style={{
+          backgroundColor: "dodgerblue",
+          // flex: 1,
+          height: "100%",
+          width: "100%",
+        }}
+      /> */}
+
+      {/* <WelcomeScreen /> */}
+      {/* <TestCardScreen /> */}
+      {/* <ViewImageScreen /> */}
+      {/* <ListingDetailsScreen listing={listingJacket} /> */}
+      <MessageListScreen />
+      {/* <MyAccountScreen /> */}
+    </ScreenContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // flex: 1 works on this, but children need w/h: 100%. Is it because GestureHandlerRootView fills up screen?
+    // height: "100%",
+    // width: "100%",
+    // backgroundColor: "dodgerblue",
+  },
+});
